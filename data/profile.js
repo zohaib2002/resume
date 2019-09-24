46d52ec8d800c00ccdedf53e7c190d8d199ddff1
Vipin Yadav <imvpn22@gmail.com>
2019-09-24 21:40:22 +0530

    minor style and content changes

--- data/profile.js
+++ data/profile.js
@@ -18,127 +18,121 @@ const profileData = {
         "Twitter": "https://twitter.com/imvpn22"
     },
     "education": [{
-            "alma": "FET, GKV, Haridwar",
-            "duration": "2014 - 2018",
-            "std": "Bachelor of Technology (Computer Science & Engineering)",
-            "score": "71%"
-        },
-        {
-            "alma": "Jwala Devi SVMIC, Allahabad",
-            "duration": "2011 - 2013",
-            "std": "Class XI - XII (PCM)",
-            "score": "90%"
-        }
+        "alma": "FET, GKV, Haridwar",
+        "duration": "2014 - 2018",
+        "std": "Bachelor of Technology (Computer Science & Engineering)",
+        "score": "71%"
+    },
+    {
+        "alma": "Jwala Devi SVMIC, Allahabad",
+        "duration": "2011 - 2013",
+        "std": "Class XI - XII (PCM)",
+        "score": "90%"
+    }
     ],
     "skills": [{
-            "graph": "false",
-            "category": "Programming Languages",
-            "topics": [
-                "C",
-                "C++",
-                "JavaScript"
-            ]
-        },
-        {
-            "graph": "false",
-            "category": "Databases",
-            "topics": [
-                "MongoDB",
-                "PostgreSQL",
-                "Oracle PL/SQL"
-            ]
-        },
-        {
-            "graph": "false",
-            "category": "Frameworks / Libraries",
-            "topics": [
-                "NodeJS",
-                "Angular",
-                "ReactJS",
-                "SCSS",
-                "LESS",
-                "Gulp",
-                "Grunt",
-                "Webpack"
-            ]
-        }
+        "graph": "false",
+        "category": "Programming Languages",
+        "topics": [
+            "C",
+            "C++",
+            "JavaScript"
+        ]
+    },
+    {
+        "graph": "false",
+        "category": "Databases",
+        "topics": [
+            "MongoDB",
+            "PostgreSQL",
+            "Oracle PL/SQL"
+        ]
+    },
+    {
+        "graph": "false",
+        "category": "Frameworks / Libraries",
+        "topics": [
+            "NodeJS",
+            "Angular",
+            "ReactJS"
+        ]
+    }
     ],
     "projects": [{
-            "title": "Restaurant Review",
-            "duration": "June - Nov 2018",
-            "link": "https://github.com/imvpn22/restaurant-review-pwa",
-            "desc": "Udacity's 'Mobile Web Specialist Restaurant Reviews App' Project for  MWS Nanodegree"
-        },
-        {
-            "title": "WhiteBoard",
-            "duration": "Jul - Aug 2017",
-            "link": "https://github.com/imvpn22/whiteboard",
-            "desc": "A simple messaging and sketch streaming web app for brainstorming sessions"
-        },
-        {
-            "title": "Jnanagni 2017",
-            "duration": "Jan - Mar 2017",
-            "link": "https://github.com/methusael13/jnanagni17",
-            "desc": "Official website for Jnanagni 2017 - FET, GKV's Annual Techfest"
-        }
+        "title": "Restaurant Review",
+        "duration": "June - Nov 2018",
+        "link": "https://github.com/imvpn22/restaurant-review-pwa",
+        "desc": "Udacity's 'Mobile Web Specialist Restaurant Reviews App' Project for  MWS Nanodegree"
+    },
+    {
+        "title": "WhiteBoard",
+        "duration": "Jul - Aug 2017",
+        "link": "https://github.com/imvpn22/whiteboard",
+        "desc": "A simple messaging and sketch streaming web app for brainstorming sessions"
+    },
+    {
+        "title": "Jnanagni 2017",
+        "duration": "Jan - Mar 2017",
+        "link": "https://github.com/methusael13/jnanagni17",
+        "desc": "Official website for Jnanagni 2017 - FET, GKV's Annual Techfest"
+    }
     ],
     "experiences": [{
-            "desc": "<b>Project Engineer</b> at  <a target='_blank' href='https://www.wipro.com/'> <strong> Wipro </strong> </a>",
-            "date": "Aug 2018 - Present",
-            "details": [
-                "Worked on various Web-Application projects, with couple of web technologies.",
-                "Currently working on a project with a tech-stack of Angular, Angular-Material and NodeJS-Express.",
-                "Responsible for implementing reusable UI/UX components and interacting with backend service.",
-            ]
-        },
-        {
-            "desc": "<b>Full-Stack Developer intern</b> at  <a target='_blank' href='https://www.venuemonk.com/'> <strong> Venuemonk </strong> </a> (<a target='_blank' href='https://drive.google.com/open?id=1ch_9x9jCp_TJwKY43FG-pCr_zV9H2iun'>Certificate: https://goo.gl/Uz1gaf</a>)",
-            "date": "Jan - Apr 2018",
-            "details": [
-                "Responsible for developing a web-application <b>Venue OnBoarding</b> from design to implementation.",
-                "Work included developing ReactJS-Redux frontend and integrating it with NodeJS-ExpressJS-MongoDB backend."
-            ]
-        },
-        {
-            "desc": "<b>Product development intern</b> at <a target='_blank' href='https://hasura.io/'> <strong> Hasura </strong> </a> (<a target='_blank' href='https://drive.google.com/file/d/0B6uOcoHYLgJ7UmlrUDlsMkVwajg/view?usp=sharing'>Certificate: https://goo.gl/8V3jBy</a>)",
-            "date": "May - Aug 2017",
-            "details": [
-                "Responsible for developing web-application using Hasura platform APIs.",
-                "Work included designing and implementing frontend application in HTML5-CSS3-JavaScript."
-            ]
-        }
+        "desc": "<strong>Project Engineer</strong> at  <a target='_blank' href='https://www.wipro.com/'> <strong> Wipro </strong> </a>",
+        "date": "Aug 2018 - Present",
+        "details": [
+            "Working on a project with a tech-stack of Angular, Angular-Material and NodeJS-Express.",
+            "Responsible for implementing reusable UI/UX components and interacting with backend service.",
+        ]
+    },
+    {
+        "desc": "<strong>Full-Stack Developer intern</strong> at  <a target='_blank' href='https://www.venuemonk.com/'> <strong> Venuemonk </strong> </a> (<a target='_blank' href='https://drive.google.com/open?id=1ch_9x9jCp_TJwKY43FG-pCr_zV9H2iun'> https://goo.gl/Uz1gaf</a>)",
+        "date": "Jan - Apr 2018",
+        "details": [
+            "Responsible for developing a web-application <strong>Venue OnBoarding</strong> from design to implementation.",
+            "Work included developing ReactJS-Redux frontend and integrating it with NodeJS-ExpressJS-MongoDB backend."
+        ]
+    },
+    {
+        "desc": "<strong>Product development intern</strong> at <a target='_blank' href='https://hasura.io/'> <strong> Hasura </strong> </a> (<a target='_blank' href='https://drive.google.com/file/d/0B6uOcoHYLgJ7UmlrUDlsMkVwajg/view?usp=sharing'> https://goo.gl/8V3jBy</a>)",
+        "date": "May - Aug 2017",
+        "details": [
+            "Responsible for developing web-application using Hasura platform APIs.",
+            "Work included designing and implementing frontend application in HTML5-CSS3-JavaScript."
+        ]
+    }
     ],
     "events": [{
-            "title": "certifications",
-            "body": [{
-                    "desc": "<strong>Mobile Web Specialist</strong>, a Nanodegree provided by <a target='_blank' href='https://in.udacity.com//'> <b>Udacity</b>  (<a target='_blank' href='https://confirm.udacity.com/RLMHXAWZ'>Certificate: https://goo.gl/RGRzVn</a>)",
-                    "date": "May - Nov 2018"
-                },
-                {
-                    "desc": "<a target='_blank' href='https://www.imad.tech'><strong>IMAD</strong></a>, a MOOC provided by <a target='_blank' href='https://onlinecourses.nptel.ac.in/'> <b>NPTEL</b> </a> and <a target='_blank' href='https://hasura.io/'> <strong> Hasura</strong> </a> (<a target='_blank' href='http://nptel.ac.in/noc/E_Certificate/linkedin/noc17-cs06/NPTEL17CS0626270067AN.jpg'>Certificate: https://goo.gl/X3HEdR</a>)",
-                    "date": "Jan - Mar 2017"
-                },
-                {
-                    "desc": "<strong>ETCS</strong>, a Summer Training provided by <a href='http://www.mnnit.ac.in/' target='_blank'> <b>MNNIT </b> Allahabad</a> (<a target='_blank' href='https://drive.google.com/file/d/0B6uOcoHYLgJ7WEVmdlJDcWVZcEE/view?usp=sharing'>Certificate link</a>)",
-                    "date": "Jun - July 2016"
-                }
-            ]
+        "title": "certifications",
+        "body": [{
+            "desc": "<strong>Mobile Web Specialist</strong>, a Nanodegree provided by <a target='_blank' href='https://in.udacity.com//'> <strong>Udacity</strong>  (<a target='_blank' href='https://confirm.udacity.com/RLMHXAWZ'> https://goo.gl/RGRzVn</a>)",
+            "date": "May - Nov 2018"
         },
         {
-            "title": "other",
-            "body": [{
-                    "desc": "<strong>NPAT</strong>(<b>N</b>ATIONAL <b>P</b>ROGRAMMING <b>A</b>PTITUDE <b>T</b>EST) <a target='_blank' href='https://npat.nptel.ac.in/npat_2017/certificate?id=117913621007613125255'>(Certificate link)</a> ",
-                    "date": "Aug 2017"
-                },
-                {
-                    "desc": "Event Organizer in <strong>Jnanagni'17</strong> (Annual Techno-Cultural fest of FET, GKV)",
-                    "date": "Mar 2017"
-                },
-                {
-                    "desc": "Conducted a workshop for 1<sup>st</sup> and 2<sup>nd</sup> year students on <strong>Linux Essentials</strong>",
-                    "date": "Oct 2016"
-                }
-            ]
+            "desc": "<a target='_blank' href='https://www.imad.tech'><strong>IMAD</strong></a>, a MOOC provided by <a target='_blank' href='https://onlinecourses.nptel.ac.in/'> <strong>NPTEL</strong> </a> and <a target='_blank' href='https://hasura.io/'> <strong> Hasura</strong> </a> (<a target='_blank' href='http://nptel.ac.in/noc/E_Certificate/linkedin/noc17-cs06/NPTEL17CS0626270067AN.jpg'> https://goo.gl/X3HEdR</a>)",
+            "date": "Jan - Mar 2017"
+        },
+        {
+            "desc": "<strong>ETCS</strong>, a Summer Training provided by <a href='http://www.mnnit.ac.in/' target='_blank'> <strong>MNNIT </strong> Allahabad</a> (<a target='_blank' href='https://drive.google.com/file/d/0B6uOcoHYLgJ7WEVmdlJDcWVZcEE/view?usp=sharing'>Certificate link</a>)",
+            "date": "Jun - July 2016"
         }
+        ]
+    },
+    {
+        "title": "other",
+        "body": [{
+            "desc": "<strong>NPAT</strong>(<strong>N</strong>ATIONAL <strong>P</strong>ROGRAMMING <strong>A</strong>PTITUDE <strong>T</strong>EST) <a target='_blank' href='https://npat.nptel.ac.in/npat_2017/certificate?id=117913621007613125255'>(Certificate link)</a> ",
+            "date": "Aug 2017"
+        },
+        {
+            "desc": "Event Organizer in <strong>Jnanagni'17</strong> (Annual Techno-Cultural fest of FET, GKV)",
+            "date": "Mar 2017"
+        },
+        {
+            "desc": "Conducted a workshop for 1<sup>st</sup> and 2<sup>nd</sup> year students on <strong>Linux Essentials</strong>",
+            "date": "Oct 2016"
+        }
+        ]
+    }
     ]
 };
